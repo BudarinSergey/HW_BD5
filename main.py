699c90b6bf57bb673f9024c4fa5a40478d1f52fb
Budarin Sergey <budarin_s@mail.ru>
2022-08-15 23:56:30 +0300

    3th commit

--- main.py
+++ main.py
@@ -1,41 +1,7 @@
 
 import psycopg2
-# conn = psycopg2.connect(database="HW_PG_Py", user="postgres", password="Sergey12")
-# with conn.cursor() as cur:
-#     cur.execute("""
-#         DROP TABLE mob_number;
-#         DROP TABLE users;
-#         """)
-#     cur.execute("""
-#         CREATE TABLE IF NOT EXISTS users(
-#             id SERIAL PRIMARY KEY,
-#             name VARCHAR(20) NOT NULL,
-#             surname VARCHAR(20) ,
-#             email VARCHAR(30) UNIQUE
-#         );
-#         """)
-#
-#     cur.execute("""
-#         CREATE TABLE IF NOT EXISTS mob_number(
-#             id SERIAL PRIMARY KEY,
-#             mob_num INTEGER NOT NULL,
-#             user_id INTEGER NOT NULL REFERENCES users(id)
-#         );
-#         """)
-#
-#     conn.commit()
-#
-#     cur.execute("""
-#         INSERT INTO users(name) VALUES('Sergey') RETURNING id, name;
-#         """)
-#     print(cur.fetchone())
-#         # conn.commit()
-#
-# conn.close()
-
 
 def create_bd(conn):
-    # conn = psycopg2.connect(database="HW_PG_Py", user="postgres", password="Sergey12")
     with conn.cursor() as cur:
         cur.execute("""
             DROP TABLE mob_number;
@@ -60,16 +26,6 @@ def create_bd(conn):
 
         conn.commit()
 
-        # cur.execute("""
-        #             INSERT INTO users(name) VALUES('Sergey') RETURNING id;
-        #             """)
-        # print(cur.fetchone())
-        # conn.commit()
-
-
-
-
-
 def add_client(conn):
     name = input("ведите имя : ")
     surname = input("введите фамилию : ")
@@ -82,8 +38,6 @@ def add_client(conn):
         print(cur.fetchone())
         conn.commit()
 
-        # cur.execute(""" SELECT * FROM users;""")
-
 def add_phone(conn):
     userid = input("Введите ID пользователя : ")
     user_number = input("Введите номер телефона : ")
@@ -92,7 +46,6 @@ def add_phone(conn):
             INSERT INTO mob_number (mob_num, user_id) VALUES (%s, %s) RETURNING mob_num""", (user_number, userid))
         print(cur.fetchone())
         conn.commit()
-
 
 
 def change_user(conn):
@@ -155,8 +108,6 @@ def finde_user(conn):
             print(cur.fetchone())
     else:
         print('Вы неправильно задали параметры')
-
-
         conn.commit()
 
 with psycopg2.connect(database="HW_PG_Py", user="postgres", password="Sergey12") as conn:
