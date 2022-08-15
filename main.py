d0f0e083f9a34c6ab1cfced95db464a8aca8d4c5
Budarin Sergey <budarin_s@mail.ru>
2022-08-15 23:47:55 +0300

    second commit

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ def add_client(conn):
 
         print(cur.fetchone())
         conn.commit()
+
         # cur.execute(""" SELECT * FROM users;""")
 
 def add_phone(conn):
@@ -94,10 +95,77 @@ def add_phone(conn):
 
 
 
+def change_user(conn):
+    userid = input("Введите ID пользователя : ")
+    name = input("ведите новое имя : ")
+    surname = input("введите новую фамилию : ")
+    email = input("введите новый e-mail : ")
+    with conn.cursor() as cur:
+        cur.execute(""" UPDATE users SET name=%s, surname=%s, email =%s WHERE id=%s; """,(name, surname, email, userid))
+
+        cur.execute("""SELECT * FROM users;
+        """)
+        print(cur.fetchall())
+        conn.commit()
+
+def del_number(conn):
+    userid = input("Введите ID пользователя : ")
+    with conn.cursor() as cur:
+        cur.execute("""DELETE FROM mob_number WHERE id=%s; """,(userid))
+
+        cur.execute("""SELECT * FROM mob_number;
+                """)
+        print(cur.fetchall())
+        conn.commit()
+
+def del_user(conn):
+    userid = input("Введите ID пользователя : ")
+    with conn.cursor() as cur:
+        cur.execute("""DELETE FROM users WHERE id=%s; """,(userid))
+
+        cur.execute("""SELECT * FROM users;
+                """)
+        print(cur.fetchall())
+        conn.commit()
+
+def finde_user(conn):
+    finde_param = input("Выберете один из параметры поиска (1 - имя, 2 - фамилия, 3- email, 4 - номер телефона ) : ")
+    if finde_param == '1':
+        name = input("ведите имя пользователя : ")
+        with conn.cursor() as cur:
+            cur.execute("""SELECT id,  name, surname, email FROM users WHERE name=%s; """,(name,))
+            print(cur.fetchone())
+    elif finde_param == '2':
+        surname = input("ведите фамилию пользователя : ")
+        with conn.cursor() as cur:
+            cur.execute("""SELECT id,  name, surname, email FROM users WHERE surname=%s; """, (surname,))
+            print(cur.fetchone())
+    elif finde_param == '3':
+        email = input("ведите email пользователя : ")
+        with conn.cursor() as cur:
+            cur.execute("""SELECT id,  name, surname, email FROM users WHERE email=%s; """, (email,))
+            print(cur.fetchone())
+
+    elif finde_param == '4':
+        mob_num1 = input("ведите номер телефона пользователя : ")
+        with conn.cursor() as cur:
+            cur.execute("""SELECT u.id,  u.name, u.surname, u.email FROM users u
+            JOIN mob_number mn ON mn.user_id = u.id
+            WHERE mn.mob_num=%s; """, (mob_num1,))
+            print(cur.fetchone())
+    else:
+        print('Вы неправильно задали параметры')
+
+
+        conn.commit()
 
 with psycopg2.connect(database="HW_PG_Py", user="postgres", password="Sergey12") as conn:
     create_bd(conn)
     add_client(conn)
     add_phone(conn)
+    # change_user(conn)
+    # del_number(conn)
+    # del_user(conn)
+    finde_user(conn)
 
 conn.close()
